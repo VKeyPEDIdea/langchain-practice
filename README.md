9985ab8a6563c54a729f89d1f8e4a956ba27f1a0
Pavel Karyapkin <karyapkin@gmail.com>
2023-12-06 20:20:56 +0600

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# Langchain practice project
+
+## Setup
+1. Run `npm ci` to install dependencies.
+2. Define OPENAI_API_KEY in .env file
+3. Run `node ./main.js` to get results;
+
+You can define your prompt and resource url in `question` constant int main.js file.
---
+++ main.js
@@ -0,0 +1,40 @@
+import { writeFile, mkdir } from 'node:fs';
+import dotenv from 'dotenv';
+import { OpenAI } from "langchain/llms/openai";
+import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+
+const question = 'В чем разница между параллельным и последовательным соединением?';
+const resourceURL = 'https://alexgyver.ru/lithium_charging/';
+
+dotenv.config();
+
+new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY });
+
+const loader = new CheerioWebBaseLoader(resourceURL);
+const logError = err => {
+  if (err) console.log(err);
+};
+
+(async () => {
+  const data = await loader.load();
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 0,
+  });
+  const splitDocs = await textSplitter.splitDocuments(data);
+  const embeddings = new OpenAIEmbeddings();
+  const vectorStore = await MemoryVectorStore.fromDocuments(
+    splitDocs,
+    embeddings
+  );
+  const relevantDocs = await vectorStore.similaritySearch(question);
+  
+  const date = Date.now();
+  mkdir('./answers', logError);
+  writeFile(`./answers/${date}.json`, JSON.stringify(relevantDocs), logError);
+
+  console.log(relevantDocs);
+})();
